a5fd316a0a27369df61fe10561a939a066ffd707
umang <umang@DESKTOP-IKB2O7A>
2024-03-25 13:06:09 -0400

    new class is added

---
+++ src/main/java/Proj002/Mavenlabs/Sample2.java
@@ -0,0 +1,11 @@
+package Proj002.Mavenlabs;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git and Github 2..");
+
+	}
+
+}
